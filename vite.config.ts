453c07beb1ceee5e0d5a1d7dbd9e3cb153c6b119
norets <norets88@gmail.com>
2024-04-12 21:08:53 +0300

    Add build modes for GH Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,30 +3,42 @@ import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
 
 const projectRootDir = resolve(__dirname)
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  base:'/icons/',
-  build: {
-    emptyOutDir: true,
-    lib: {
-      // Could also be a dictionary or array of multiple entry points
-      entry: resolve(projectRootDir, 'src/lib.ts'),
-      name: 'lib',
-      // the proper extensions will be added
-      fileName: 'lib',
-      formats: ['es']
-    },
-    rollupOptions: {
-      // make sure to externalize deps that shouldn't be bundled
-      // into your library
-      external: ['vue'],
-      output: {
-        // Provide global variables to use in the UMD build
-        // for externalized deps
-        globals: {
-          vue: 'Vue'
+
+export default defineConfig(({mode})=>{
+  if (mode === 'lib') {
+    return{
+      plugins: [vue()],
+      build: {
+        emptyOutDir: true,
+        lib: {
+          // Could also be a dictionary or array of multiple entry points
+          entry: resolve(projectRootDir, 'src/lib.ts'),
+          name: 'lib',
+          // the proper extensions will be added
+          fileName: 'lib',
+          formats: ['es']
+        },
+        rollupOptions: {
+          // make sure to externalize deps that shouldn't be bundled
+          // into your library
+          external: ['vue'],
+          output: {
+            // Provide global variables to use in the UMD build
+            // for externalized deps
+            globals: {
+              vue: 'Vue'
+            }
+          }
         }
+      }
+    }
+  }
+  if (mode === 'site') {
+    return{
+      plugins: [vue()],
+      base:'/icons/',
+      build:{
+        outDir:resolve(projectRootDir, 'site'),
       }
     }
   }
